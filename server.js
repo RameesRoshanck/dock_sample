1c9b165136aaa71682b68bc540c49a104f9f785a
Mohamed Ramees Roshan ck <90952452+RameesRoshanck@users.noreply.github.com>
2024-05-03 16:18:54 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app =express()
+
+app.get("/",(req,res)=>{
+    res.send("Hello world")
+})
+
+let port =3000
+
+app.listen(port,()=>{
+    console.log(`listening to port ${port}`);
+})
